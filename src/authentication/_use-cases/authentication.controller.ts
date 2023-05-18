ca5039fd8755bd281b21f4dd91be243f986a9946
chabrik abdellatif <abdellatif.chabrik.fycomputing@gmail.com>
2023-05-18 19:38:18 +0100

    feat: :sparkles: add secret proxy

--- src/authentication/_use-cases/authentication.controller.ts
+++ src/authentication/_use-cases/authentication.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 import { UserModel } from 'src/models/user.model';
 import { MyError } from '../../_core/_business/baseError.error';
 import { loginRequestDto } from './login/login-request.dto';
@@ -21,11 +22,24 @@ export class AuthenticationController {
     return await this.loginUseCase.login(userInfo);
   }
 
+  /*@Post('/login2')
+  public login2() {
+    createProxyMiddleware({
+      target: 'http://localhost:3002',
+      changeOrigin: true,
+      //pathFilter: '/authentication',
+      onProxyReq: (clientRequest, req, res) => {
+        const request = clientRequest;
+        clientRequest.setHeader('gateWaykey', 'chabrik');
+        console.log('req');
+      },
+    });
+  }*/
+
   @Post('/signup')
   public async createUser(
     @Body() user: CreateUserRequestDto,
   ): Promise<MyError | UserModel> {
-
     return await this.signUpUseCase.signUp(user);
   }
 }
